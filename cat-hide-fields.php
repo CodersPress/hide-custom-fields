6a28d0614d5c0b82f0cd0a6df3bb3108f5ca0b82
Martin Doyle <support@coderspress.com>
2015-09-13 00:44:51 +0100

    update

--- cat-hide-fields.php
+++ cat-hide-fields.php
@@ -2,8 +2,8 @@
 Plugin Name: Show Only Per Category 
 Plugin URI: http://coderspress.com/forum/hide-custom-fields/
 Description: Shows only selected fields on Add Listing template, based on Category. PremiumPress 6.6.5 | 8.4
-Version: 2015.0905
-Updated: 5th September 2015 
+Version: 2015.0913
+Updated: 13th September 2015 
 Author: sMarty
 Author URI: http://coderspress.com
 WP_Requires: 3.8.1
@@ -261,12 +261,13 @@ var estep = '<?php echo $step;?>';
             jQuery.each(db_conditions, function (i, val) {
                 var check_cat = val.category;
                 var FieldToTreat = val.field_label;
+				var FieldsToTreat = jQuery(FieldToTreat).text();
                 if (category == check_cat) {
-                    jQuery(".customfield").filter(":contains(" + FieldToTreat + ")").show();
+                    jQuery(".customfield").filter(":contains(" + FieldsToTreat + ")").show();
 					fieldNames += val.field_label;
 				} 
 				if (fieldNames.indexOf(val.field_label) < 0){
-				jQuery(".customfield").filter(":contains(" + FieldToTreat + ")").hide().find("input").attr('id','');
+				jQuery(".customfield").filter(":contains(" + FieldsToTreat + ")").hide().find("input, textarea").attr('id','');
                 jQuery('form .btn-primary').attr('disabled', false);
 				}
             });
